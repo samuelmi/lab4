e3a356394dd77f76dc5a85db481916c184188ac7
Sam <samuelmi@live.unc.edu>
2021-11-03 16:40:17 -0400

    Initialized repo

---
+++ Lab4.srcs/sources_1/new/vgadisplaydriver.sv
@@ -0,0 +1,28 @@
+//////////////////////////////////////////////////////////////////////////////////
+//
+// Montek Singh
+// 9/8/2021
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+`timescale 1ns / 1ps
+`default_nettype none
+`include "display640x480.vh"    // Replace this with the 640x480 values for actual implementation
+
+module vgadisplaydriver(
+    input wire clk,
+    output wire [3:0] red, green, blue,
+    output wire hsync, vsync
+    );
+
+   wire [`xbits-1:0] x;
+   wire [`ybits-1:0] y;
+   wire activevideo;
+
+   vgatimer myvgatimer(clk, hsync, vsync, activevideo, x, y);
+   
+   assign red[3:0]   = (activevideo == 1) ? x[5:2] : 4'b0;
+   assign green[3:0] = (activevideo == 1) ? y[5:2] : 4'b0;
+   assign blue[3:0]  = (activevideo == 1) ? (x[5:2]+y[5:2]) : 4'b0;
+
+endmodule
